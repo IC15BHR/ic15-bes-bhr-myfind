716dd8c1a691bae86530344a9eafad1563d1fabf
Michael Riedmann <michael_riedmann@live.com>
2016-02-09 22:43:51 +0100

    removed some unnecessary stuff

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
 
 void exit_with_error(int return_code, char *text);
 void print_error(char *text);
 
-void do_file(const char * file_name, const char * const * parms);
-void do_dir(const char * dir_name, const char * const * parms);
+void do_file(const char * file_name);
+void do_dir(const char * dir_name);
+void do_print(const char * file_name);
 
 //argv: -printf
 int main(int argc, char *argv[])
@@ -22,28 +25,30 @@ int main(int argc, char *argv[])
     char* file_name = argv[1];
     printf("Got target dir: %s\n", file_name);
 
-    do_file(file_name, NULL);
+    do_file(file_name);
 }
 
-void do_file(const char * file_name, const char * const * parms)
+void do_file(const char * file_name)
 {
     printf("Call do_file with %s\n", file_name);
 
-    for(int i = 0; parms[i] != NULL; i++)
-        printf("  %s\n", parms[i]);
+    DIR *directory = opendir(file_name);
+    if(directory != NULL) {
+        do_dir(file_name);
+        closedir(directory);
+    }
+
+    do_print(file_name);
 }
 
-void do_dir(const char * dir_name, const char * const * parms)
+void do_dir(const char * dir_name)
 {
     printf("Call do_dir with %s\n", dir_name);
-
-    for(int i = 0; parms[i] != NULL; i++)
-        printf("  %s\n", parms[i]);
 }
 
-void do_print(void)
+void do_print(const char * file_name)
 {
-
+    printf("%s\n", file_name);
 }
 
 void exit_with_error(int return_code, char *text) {
