ca63321d458357da0c92ca1a10a9a55b158798a6
Michael Riedmann <michael_riedmann@live.com>
2016-02-10 17:35:48 +0100

    added listing feature

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
 #include "helpers.h"
 
 #define ARG_MIN 2
@@ -10,7 +12,11 @@ void do_file(const char * file_name, const char * const * parms);
 void do_dir(const char * dir_name, const char * const * parms);
 
 void do_print(const char *name);
+void do_list(const char * file_name);
+
 void do_help(void);
+
+void do_params(const char *name, const char *const *pString);
 
 static const char ARG_PRINT[] = "-print";
 static const char ARG_LIST[] = "-ls";
@@ -33,7 +39,7 @@ int main(int argc, char* argv[])
     }
 
     argv[argc] = NULL;
-    if(!is_str_equal(argv[argc-1], ARG_PRINT)) //add default print to end if none is given
+    if(!is_str_equal(argv[argc-1], ARG_PRINT) && !is_str_equal(argv[argc-1], ARG_LIST)) //add default print to end if none is given
         strcpy(argv[argc-1], ARG_PRINT);
 
     do_file(argv[1], (const char**)&argv[2]);
@@ -52,11 +58,17 @@ void do_file(const char * file_name, const char * const * parms)
         closedir(dir);
     }
 
+    do_params(file_name, parms);
+}
+
+void do_params(const char *file_name, const char *const *parms) {
     const char * p;
     int i = 0;
     while((p = parms[i++]) != NULL) {
         if (is_str_equal(parms[0], ARG_PRINT))
             do_print(file_name);
+        else if(is_str_equal(parms[0], ARG_LIST))
+            do_list(file_name);
     }
 }
 
@@ -69,14 +81,14 @@ void do_dir(const char * dir_name, const char * const * parms)
             if(is_str_equal(dp->d_name, ".") || is_str_equal(dp->d_name, ".."))
                 continue;
 
-            char buf[FILENAME_MAX];
-            snprintf(buf, sizeof buf, "%s/%s", dir_name, dp->d_name);
-            printf("%s\n", buf);
+            char path[FILENAME_MAX];
+            snprintf(path, sizeof path, "%s/%s", dir_name, dp->d_name);
+            do_params(dir_name, parms);
 
             if(dp->d_type == DT_DIR)
-                do_dir(buf, parms);
+                do_dir(path, parms);
             else
-                do_file(buf, parms);
+                do_file(path, parms);
         }
         closedir(dfd);
     } else {
@@ -89,11 +101,26 @@ void do_print(const char * file_name)
     printf("%s\n", file_name);
 }
 
+void do_list(const char * file_name)
+{
+    struct stat s;
+    struct tm lt;
+
+    stat(file_name, &s);
+
+    localtime_r(&s.st_mtime, &lt);
+    char timetext[80];
+    strftime(timetext, 80, "%b %d %H:%M", &lt);
+
+    printf("%8lu %8lu %s %2lu %6d %6d %13s %s\n", s.st_ino, s.st_size, "P", s.st_nlink, s.st_uid, s.st_gid, timetext, file_name); //inode blocksize permissions linkcount owner group lastmoddate dirname
+}
+
 void do_help(void)
 {
     printf(
             "Usage: find <dir> <expressions>\n\nExpressions:"
             "  -print     returns name of directory\n"
+            "  -ls        returns formatted list\n"
     );
 }
 
