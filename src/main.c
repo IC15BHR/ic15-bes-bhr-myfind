a06fcd00920af79c819dc5eb4b41fb7e2b26f3c6
Michael Riedmann <michael_riedmann@live.com>
2016-02-10 14:32:25 +0100

    changed implementation to fit given approach

--- src/main.c
+++ src/main.c
@@ -6,35 +6,51 @@
 
 void exit_with_error(int return_code, char *text);
 void print_error(char *text);
-int is_str_equal(char *a, char *b);
+int is_str_equal(const char *a, const char *b);
 
-void do_file(const char * file_name);
-void do_dir(const char * dir_name);
-void do_print(const char * file_name);
+void do_file(const char * file_name, const char * const * parms);
+void do_dir(const char * dir_name, const char * const * parms);
 
-//argv: -printf
-int main(int argc, char *argv[])
+void do_printf(const char *name, const char *const *parms);
+
+const char* ARG_PRINTF = "-printf";
+const char* ARG_PRINTF_DEFAULT = "%s\n";
+
+int main(int argc, const char * const * argv)
 {
     if(argc < 2)
         exit_with_error(1, "Too few arguments given!");
 
-    char* file_name = argv[1];
-    printf("Got target dir: %s\n", file_name);
-
-    do_dir(file_name);
+    do_file(argv[1], &argv[2]);
 }
 
-void do_file(const char * file_name)
+void do_file(const char * file_name, const char * const * parms)
 {
-    printf("Call do_file with %s\n", file_name);
+    if(parms == NULL || parms[0] == NULL || parms[1] == NULL)
+        do_printf(file_name, NULL);
+    else if(is_str_equal(parms[0], ARG_PRINTF))
+        do_printf(file_name, &parms[1]);
 
-    do_print(file_name);
+    DIR *dir = opendir(file_name);
+    if(dir != NULL){
+        do_dir(file_name, parms);
+        closedir(dir);
+    }
 }
 
-void do_dir(const char * dir_name)
-{
-    printf("Call do_dir with %s\n", dir_name);
+void do_printf(const char * file_name, const char * const * parms) {
+    const char* format;
 
+    if(parms == NULL || parms[1] == NULL)
+        format = ARG_PRINTF_DEFAULT;
+    else
+        format = parms[1];
+
+    printf(format, file_name);
+}
+
+void do_dir(const char * dir_name, const char * const * parms)
+{
     struct dirent *dp;
     DIR *dfd = opendir(dir_name);
     if(dfd != NULL) {
@@ -49,19 +65,14 @@ void do_dir(const char * dir_name)
             printf("%s\n", buf);
 
             if(dp->d_type == DT_DIR)
-                do_dir(buf);
+                do_dir(buf, parms);
             else
-                do_file(buf);
+                do_file(buf, parms);
         }
         closedir(dfd);
     } else {
-        do_file(dir_name);
+        do_file(dir_name, parms);
     }
-}
-
-void do_print(const char * file_name)
-{
-    printf("%s\n", file_name);
 }
 
 void exit_with_error(int return_code, char *text) {
@@ -73,7 +84,7 @@ void print_error(char *text) {
     fprintf(stderr, "ERROR: %s\n", text);
 }
 
-int is_str_equal(char *a, char *b)
+int is_str_equal(const char *a, const char *b)
 {
     return strcmp(a,b) == 0;
 }
